05b11bdc9af256509b8c615a11ec2f718adc137d
Alejo1387 <trabajoalgoritmos02@gmail.com>
2026-03-14 22:34:07 -0500

    added tests for spaces and arrays

--- test/test_lexer.py
+++ test/test_lexer.py
@@ -9,3 +9,19 @@ def test_simple_object_tokens():
     assert tokens[0].type == TokenType.LEFT_BRACE
     assert tokens[1].type == TokenType.RIGHT_BRACE
     assert tokens[2].type == TokenType.EOF
+
+def test_array_tokens():
+    lexer = Lexer("[]")
+    tokens = lexer.tokenize()
+
+    assert tokens[0].type == TokenType.LEFT_BRACKET
+    assert tokens[1].type == TokenType.RIGHT_BRACKET
+    assert tokens[2].type == TokenType.EOF
+
+def test_whitespace_ignored():
+    lexer = Lexer("{   }")
+    tokens = lexer.tokenize()
+
+    assert tokens[0].type == TokenType.LEFT_BRACE
+    assert tokens[1].type == TokenType.RIGHT_BRACE
+    assert tokens[2].type == TokenType.EOF
