700bef8cf915da56997d0e1c2f120b604369b93d
Alejo1387 <trabajoalgoritmos02@gmail.com>
2026-03-14 22:33:08 -0500

    added a identify for spaces and arrays

--- src/json_parser/lexer.py
+++ src/json_parser/lexer.py
@@ -13,11 +13,21 @@ class Lexer:
         while self.position < len(self.text):
             char = self.text[self.position]
 
+            if char.isspace():
+                self.position += 1
+                continue
+
             if char == "{":
                 tokens.append(Token(TokenType.LEFT_BRACE))
 
             elif char == "}":
                 tokens.append(Token(TokenType.RIGHT_BRACE))
+
+            elif char == "[":
+                tokens.append(Token(TokenType.LEFT_BRACKET))
+
+            elif char == "]":
+                tokens.append(Token(TokenType.RIGHT_BRACKET))
 
             elif char == ":":
                 tokens.append(Token(TokenType.COLON))
