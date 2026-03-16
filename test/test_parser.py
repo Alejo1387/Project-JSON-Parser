5e92190c2dfc9a60d71724648857d8926aa6c8c5
Alejo1387 <trabajoalgoritmos02@gmail.com>
2026-03-15 21:32:23 -0500

    test for parser object

--- test/test_parser.py
+++ test/test_parser.py
@@ -10,3 +10,12 @@ def test_parse_number():
     result = parser.parse()
 
     assert result == 123
+
+def test_parse_simple_object():
+    lexer = Lexer('{"a":1}')
+    tokens = lexer.tokenize()
+
+    parser = Parser(tokens)
+    result = parser.parse()
+
+    assert result == {"a": 1}
